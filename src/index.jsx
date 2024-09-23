56592b2dbe7ab6044472b6a88c3a2357e519fc17
Januwa Putra <januwaputra29@gmail.com>
2024-09-23 15:44:19 +0700

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+
+
+import { createRoot } from "react-dom/client";
+import ContactApp from "./components/ContactApp";
+
+// Style
+import './styles/style.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<ContactApp />);
