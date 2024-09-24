25ba183ba42fa8081f70d4bc38fff492a2d49ee2
Januwa Putra <66652223+JanuwaPutra@users.noreply.github.com>
2024-09-24 15:39:50 +0700

    Update ContactApp.jsx

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -1,16 +1,51 @@
 import ContactList from "./ContactList";
 import { getData } from "../utilis/data";
 import React from 'react';
+import ContactInput from './ContactInput';
 
-function ContactApp() {
-  const contacts = getData();
+class ContactApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: getData(),
+    }
+  
+    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onAddContactHandler = this.onAddContactHandler.bind(this);
+  }
+  
+  onDeleteHandler(id) {
+    const contacts = this.state.contacts.filter(contact => contact.id !== id);
+    this.setState({ contacts });
+  }
 
-  return (
-    <div className="contact-app">
-      <h1>Daftar Kontak</h1>
-      <ContactList contacts={contacts} />
-    </div>
-  );
-}
-
-export default ContactApp;
+  onAddContactHandler({ name, tag }) {
+    this.setState((prevState) => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          {
+            id: +new Date(),
+            name,
+            tag,
+            imageUrl: '/images/default.jpg',
+          }
+        ]
+      }
+    });
+  }
+  
+  render() {
+    return (
+      <div className="contact-app">
+        <h1>Daftar Kontak</h1>
+        <h1>Aplikasi Kontak</h1>
+        <h2>Tambah Kontak</h2>
+        <ContactInput addContact={this.onAddContactHandler} />
+        <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
+      </div>
+    );
+  }
+ }
+  
+ export default ContactApp;
